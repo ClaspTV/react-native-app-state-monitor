e784821f46a0688ed684edb4d939a5327712bc04
Shiva Reddy <shiva@vizbee.tv>
2025-03-03 17:46:27 +0530

    Improved logging

--- AppStateMonitor.js
+++ AppStateMonitor.js
@@ -21,9 +21,9 @@ class AppStateMonitor {
     this._subscription = AppStateEventEmitter.addListener(
       'rnAppStateDidChange',
       (event) => {
+        console.log(`AppStateMonitor: event - ${JSON.stringify(event)}, currentState - ${this._currentState}`);
         if (event && event.app_state && event.app_state !== this._currentState) {
           this._currentState = event.app_state;
-          console.log('AppStateMonitor: vzbAppStateDidChange:', this._currentState);
           this._emitChange(this._currentState);
         }
       }
